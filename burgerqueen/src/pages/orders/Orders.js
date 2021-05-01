a79cc8e81330f50a4e32ecc73ff450325559bfba
AnaliMtzart <analimjack@gmail.com>
2021-04-30 22:23:32 -0500

    Vista del mesero funcionando css

--- burgerqueen/src/pages/orders/Orders.js
+++ burgerqueen/src/pages/orders/Orders.js
@@ -1,35 +1,111 @@
-import React from 'react';
-import './Orders.css';
-import Header from '../../components/header/Header';
-import Navbar from '../../components/navBar/Navbar';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import "./Orders.css";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navBar/Navbar";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import imag from "../../assets/img/hamburguesa-doble.jpg";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-
-    offset: theme.mixins.toolbar,
-
+  size: {
+    width: "394px",
+  },
+  offset: theme.mixins.toolbar,
 }));
 
 const Orders = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            <div>
-                <Header />
-                <Navbar/>
-            </div>
+  //   let [products, setProducts] = useState();
+  //   let getData = async () => {
+  //     let url = "http://localhost:3004/db";
+  //     let getFectchData = await fetch(url).then((resul) => resul.json());
+  //     //console.log(getFectchData);
+  //     setProducts(getFectchData);
+  //   };
 
+  //   useEffect(() => {
+  //     getData();
+  //   }, []);
 
-            <div className={classes.offset}></div>
+  //   function sayHello() { //probar click boton
+  //     // alert('Hello!');
+  //     console.log('clicked')
+  //   }
+  // onClick={sayHello}
+
+  return (
+    <div>
+      {/* <div> */}
+      <Header />
+      <Navbar />
+      {/* </div> */}
+      <div className="name">
+        <TextField className={classes.size} label="Cliente" />
+      </div>
+      <div className="number">
+        <TextField label="Número de mesa" type="number" variant="outlined" />
+        <InputLabel shrink>Ingresa solo números</InputLabel>
+      </div>
+
+      <div id="menuContent">
+        <div className="btnsMenu">
+          <Button id="btnAll" size="large" variant="outlined" color="primary">
+            TODO
+          </Button>
+          <Button
+            id="btnBreakfast"
+            size="large"
+            variant="outlined"
+            color="primary"
+          >
+            BREAKFAST
+          </Button>
+          <Button
+            id="btnDinnner"
+            size="large"
+            variant="outlined"
+            color="primary"
+          >
+            DINNER
+          </Button>
         </div>
-    )
-}
+        <div className="tableMenu">
+          {/* iterar sobre el arreglo se modificara al consumir json de api*/}
+          <button type="submit" id="btnItem">
+            <img src={imag} alt="" id="itemMenu" />
+          </button>
+          <button type="submit" id="btnItem">
+            <img src={imag} alt="" id="itemMenu" />
+          </button>
+          <button type="submit" id="btnItem">
+            <img src={imag} alt="" id="itemMenu" />
+          </button>
+          <button type="submit" id="btnItem">
+            <img src={imag} alt="" id="itemMenu" />
+          </button>
+          <button type="submit" id="btnItem">
+            <img src={imag} alt="" id="itemMenu" />
+          </button>
+          <button type="submit" id="btnItem">
+            <img src={imag} alt="" id="itemMenu" />
+          </button>
+        </div>
+      </div>
 
-export default Orders
+      <div id="ordercontent">
+        <h2>BURGER QUEEN ORDER</h2>
+        <div id="nameClient">
+          <h4>María Conchita</h4>
+        </div>
+        <div id="order"></div>
+      </div>
+
+      <div className={classes.offset}></div>
+    </div>
+  );
+};
+
+export default Orders;
