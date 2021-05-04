30e48cb3c47a3c847770c60a39daed1b56b586ed
AnaliMtzart <analimjack@gmail.com>
2021-05-04 12:30:49 -0500

    funcionalidad botones menu

--- burgerqueen/src/pages/orders/Orders.js
+++ burgerqueen/src/pages/orders/Orders.js
@@ -1,13 +1,16 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+// import SendIcon from '@material-ui/icons/Send';
 import "./Orders.css";
 
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navBar/Navbar";
-import Item from "./Item";
+import Item from "../../components/Item";
+
+import ReactDOM from 'react-dom';
 
 const useStyles = makeStyles((theme) => ({
   size: {
@@ -19,33 +22,138 @@ const useStyles = makeStyles((theme) => ({
 const Orders = () => {
   const classes = useStyles();
 
-    let [products, setProducts] = useState();
-    let handleGetData = async () => {
-      let url = "http://localhost:3000/products";
-      let getFectchData = await fetch(url).then((resul) => resul.json());
-      setProducts(getFectchData);
-    };
 
-    useEffect(() => {
-      handleGetData();
-    }, []);
+////////////////////////////////
+
+  let [products, setProducts] = useState();
+
+  let handleGetData = async () => {
+    let url = "http://localhost:3000/products";
+    let getFectchData = await fetch(url).then((result) => result.json());
+    setProducts(getFectchData);
+
+    ReactDOM.render(
+      <React.StrictMode>    
+          {products && //&& para saber si existe
+              products.map(product => (
+                <Item product={product} key={product.id} />
+              ))}
+      </React.StrictMode>,
+      document.getElementById('tableMenu')
+    );
+  };
+
+  useEffect(() => {
+    handleGetData();
+  }, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+//////////////////////////////////
+
+let [breakfasts, setBreakfasts] = useState();
+
+let handleBreakfast = async () => {
+  let url = "http://localhost:3000/products";
+  let getFectchData = await fetch(url).then((result) => result.json());
+  let filterBreakfast= getFectchData.filter((element) => element.type === 'Breakfast');
+  setBreakfasts(filterBreakfast);
+
+  ReactDOM.render(
+    <React.StrictMode>    
+        {breakfasts && //&& para saber si existe
+            breakfasts.map(product => (
+              <Item product={product} key={product.id} />
+            ))}
+    </React.StrictMode>,
+    document.getElementById('tableMenu')
+  );
+};
+
+useEffect(() => {
+  handleBreakfast();
+}, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+
+//////////////////////////////////
+
+
+let [dinners, setDinner] = useState();
+
+let handleDinner = async () => {
+  let url = "http://localhost:3000/products";
+  let getFectchData = await fetch(url).then((result) => result.json());
+  let filterDinner= getFectchData.filter((element) => element.type === 'Dinner');
+  setDinner(filterDinner);
+
+  ReactDOM.render(
+    <React.StrictMode>    
+        {dinners && //&& para saber si existe
+            dinners.map(product => (
+              <Item product={product} key={product.id} />
+            ))}
+    </React.StrictMode>,
+    document.getElementById('tableMenu')
+  );
+};
+
+useEffect(() => {
+  handleDinner();
+}, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+
+//////////////////////////////////
+
+const [clients, setClients] = useState({
+  nameClient: '',
+})
+
+const handleInputChange = (event) => {
+  // console.log(event.target.name)
+  setClients({
+    ...clients,
+    [event.target.name] : event.target.value
+  })
+}
+
+// const enviarDatos = (event) => {
+//   event.preventDefault()
+//   console.log('enviando datos...' + clients.nameClient )
+// }
+
+////////////////////////
+
+const [tables, setTables] = useState({
+  table: '',
+})
+
+const handleNumberChange = (event) => {
+  // console.log(event.target.name)
+  setTables({
+    ...tables,
+    [event.target.id] : event.target.value
+  })
+}
+
+////////////////////////
+
 
   return (
     <div>
       <Header />
       <Navbar />
-      <div className="name">
-        <TextField className={classes.size} label="Cliente" />
+      <div className="nameOfClient">
+        <TextField className={classes.size} label="Cliente" onChange={handleInputChange} name="nameClient" />
+        {/* <button onClick={enviarDatos} type="submit" id="btnAgregar"><SendIcon color="primary" style={{ fontSize: 30 }} /></button>  */}
       </div>
-      <div className="number">
-        <TextField label="Número de mesa" type="number" variant="outlined" />
+      <div className="numberOfTable">
+        <TextField label="Número de mesa" type="number" variant="outlined" onChange={handleNumberChange} id="table"/>
       </div>
       <div id="menuContent">
         <div className="btnsMenu">
-          <Button id="btnAll" size="large" variant="outlined" color="primary">
+          <Button onClick={handleGetData} id="btnAll" size="large" variant="outlined" color="primary">
             TODO
           </Button>
           <Button
+            onClick={handleBreakfast}
             id="btnBreakfast"
             size="large"
             variant="outlined"
@@ -54,6 +162,7 @@ const Orders = () => {
             DESAYUNO
           </Button>
           <Button
+          onClick={handleDinner}
             id="btnDinnner"
             size="large"
             variant="outlined"
@@ -62,25 +171,24 @@ const Orders = () => {
             COMIDAS
           </Button>
         </div>
-        <div className="tableMenu">
-          {/* iterar sobre el arreglo se modificara al consumir json de api*/}
-        {products&&products.map((unsoloproducto, index)=><Item
-        info={unsoloproducto}
-        key={index}
-        />)}
+        <div className="tableMenu" id="tableMenu">
+        {/* {products && //&& para saber si existe
+            products.map(product => (
+              <Item product={product} key={product.id} />
+            ))} */}
         </div>
       </div>
       <div id="ordercontent">
         <h2>ORDENES BURGER QUEEN</h2>
-        <div id="nameClient">
-          <h4>María Conchita</h4>
+        <div id="infoClient">
+          {/* <h4>María Conchita</h4> */}
+          <h4>Cliente: {clients.nameClient}</h4>
+          <h5>Número de mesa: {tables.table}</h5>
         </div>
-        <div id="order">
-          {/* aqui va cada pedido */}
-        </div>
+        <div id="order">{/* aqui va cada pedido */}</div>
         <h3>TOTAL: 150</h3>
         <Button variant="contained" color="secondary" id="btnSend">
-            ENVIAR A COCINA
+          ENVIAR A COCINA
         </Button>
       </div>
 
